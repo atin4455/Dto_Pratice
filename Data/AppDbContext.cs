2973298c5f2a849cd9680a7fe5020a2057a68568
祖程 <atinzulin@hotmail.com>
2026-04-14 16:08:08 +0800

    建立 migration 並 database update，把 Users 表實際建立出來

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,4 +6,15 @@ namespace Dto_Pratice.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<UserEntity> Users => Set<UserEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>(entity =>
+        {
+            entity.ToTable("Users");
+            entity.HasKey(x => x.Id);
+            entity.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            entity.Property(x => x.Email).HasMaxLength(200).IsRequired();
+        });
+    }
 }
---
+++ Migrations/20260414080604_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dto_Pratice.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                IF OBJECT_ID(N'[dbo].[Users]', N'U') IS NULL
+                BEGIN
+                    CREATE TABLE [dbo].[Users] (
+                        [Id] int NOT NULL IDENTITY,
+                        [Name] nvarchar(100) NOT NULL,
+                        [Email] nvarchar(200) NOT NULL,
+                        CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+                    );
+                END
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                IF OBJECT_ID(N'[dbo].[Users]', N'U') IS NOT NULL
+                BEGIN
+                    DROP TABLE [dbo].[Users];
+                END
+                """);
+        }
+    }
+}
